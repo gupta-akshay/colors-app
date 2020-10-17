0674e149383768a3e3037972f8347622b7802af4
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 20:06:38 +0530

    Add responsiveness

--- src/styles/DraggableColorBoxStyles.js
+++ src/styles/DraggableColorBoxStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes"
+
 const styles = {
   root: {
     width: "20%",
@@ -11,22 +13,34 @@ const styles = {
       color: "white",
       transform: "scale(1.5)",
     },
-    boxContent: {
-      position: "absolute",
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "20%",
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "10%",
+    },
+    [sizes.down("sm")]: {
       width: "100%",
-      left: "0",
-      bottom: "0",
-      padding: "10px",
-      color: "rgba(0, 0, 0, 0.5)",
-      letterSpacing: "1px",
-      textTransform: "uppercase",
-      fontSize: "12px",
-      display: "flex",
-      justifyContent: "space-between",
+      height: "5%",
     },
-    deleteIcon: {
-      transition: "all 0.3s ease-in-out",
-    },
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out",
   },
 }
 
