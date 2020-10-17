f8658d14995e5f78ceaceb482a1036a606422c09
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 19:03:28 +0530

    Refactor NewPaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -2,18 +2,15 @@ import React, { Component } from "react"
 import classNames from "classnames"
 import { withStyles } from "@material-ui/core/styles"
 import Drawer from "@material-ui/core/Drawer"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import Divider from "@material-ui/core/Divider"
 import IconButton from "@material-ui/core/IconButton"
-import MenuIcon from "@material-ui/icons/Menu"
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
 import Button from "@material-ui/core/Button"
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator"
 import { ChromePicker } from "react-color"
 import { arrayMove } from "react-sortable-hoc"
+import PaletteFormNav from "./PaletteFormNav"
 import DraggableColorList from "./DraggableColorList"
 
 const drawerWidth = 400
@@ -88,7 +85,6 @@ class NewPaletteForm extends Component {
       currentColor: "teal",
       newColorName: "",
       colors: this.props.palettes[0].colors,
-      newPaletteName: "",
     }
   }
 
@@ -100,11 +96,6 @@ class NewPaletteForm extends Component {
     )
     ValidatorForm.addValidationRule("isColorUnique", (value) =>
       this.state.colors.every(({ color }) => color !== this.state.currentColor)
-    )
-    ValidatorForm.addValidationRule("isPaletteNameUnique", (value) =>
-      this.props.palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
     )
   }
 
@@ -146,8 +137,8 @@ class NewPaletteForm extends Component {
     this.setState({ colors: [...colors, randomColor] })
   }
 
-  handleSubmit = () => {
-    const { colors, newPaletteName } = this.state
+  handleSubmit = (newPaletteName) => {
+    const { colors } = this.state
     const { history, savePalette } = this.props
     const newPalette = {
       paletteName: newPaletteName,
@@ -172,7 +163,7 @@ class NewPaletteForm extends Component {
   }
 
   render() {
-    const { classes, maxColors } = this.props
+    const { classes, maxColors, palettes } = this.props
     const {
       open,
       colors,
@@ -184,41 +175,13 @@ class NewPaletteForm extends Component {
 
     return (
       <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          color="default"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Persistent drawer
-            </Typography>
-            <ValidatorForm onSubmit={this.handleSubmit}>
-              <TextValidator
-                label="Palette Name"
-                value={newPaletteName}
-                name="newPaletteName"
-                onChange={this.handleChange}
-                validators={["required", "isPaletteNameUnique"]}
-                errorMessages={["Enter Palette Name", "Name already used"]}
-              />
-              <Button variant="contained" color="primary" type="submit">
-                Save Palette
-              </Button>
-            </ValidatorForm>
-          </Toolbar>
-        </AppBar>
+        <PaletteFormNav
+          open={open}
+          classes={classes}
+          palettes={palettes}
+          handleSubmit={this.handleSubmit}
+          handleDrawerOpen={this.handleDrawerOpen}
+        />
         <Drawer
           className={classes.drawer}
           variant="persistent"
