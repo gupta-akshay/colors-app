2b61bef4a4ac9d6f02944c2cdb47902af2a60a07
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 15:40:27 +0530

    Add routing + styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,16 +1,58 @@
 import React from "react"
+import { withStyles } from "@material-ui/styles"
 import { Link } from "react-router-dom"
 import MiniPalette from "./MiniPalette"
 
-const PaletteList = ({ palettes }) => {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+}
+
+const PaletteList = ({ palettes, classes, history }) => {
+  const goToPalette = (id) => {
+    history.push(`/palette/${id}`)
+  }
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map((palette) => (
-        <MiniPalette {...palette} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map((palette) => (
+            <MiniPalette
+              {...palette}
+              handleClick={() => goToPalette(palette.id)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default PaletteList
+export default withStyles(styles)(PaletteList)
