200574284bcb6cc7c20d0320b9d48191a0e1efa6
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 19:45:43 +0530

    Refactor code + styles

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -11,49 +11,7 @@ import { arrayMove } from "react-sortable-hoc"
 import PaletteFormNav from "./PaletteFormNav"
 import ColorPickerForm from "./ColorPickerForm"
 import DraggableColorList from "./DraggableColorList"
-
-const drawerWidth = 400
-
-const styles = (theme) => ({
-  root: {
-    display: "flex",
-  },
-
-  hide: {
-    display: "none",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
-  },
-  content: {
-    flexGrow: 1,
-    height: "calc(100vh - 64px)",
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-})
+import styles from "./styles/NewPaletteFormStyles"
 
 class NewPaletteForm extends Component {
   static defaultProps = {
