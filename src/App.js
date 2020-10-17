4a5ae40b110b36289279fd549c4a491607d9b26b
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 15:13:53 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from "react"
+import React, { Component } from "react"
+import { Route, Switch } from "react-router-dom"
 import Palette from "./Pallete"
 import seedColors from "./seedColors"
-import { generatePalette } from './colorHelpers'
+import { generatePalette } from "./colorHelpers"
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[0])} />
-    </div>
-  )
+class App extends Component {
+  findPalette = (id) => {
+    return seedColors.find((palette) => palette.id === id)
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+    )
+  }
 }
 
 export default App
