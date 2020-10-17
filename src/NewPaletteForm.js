03b75d63c35949dd89c70260d73cc5da0728a245
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 19:21:15 +0530

    Style Color Picker form & Nav form

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -18,24 +18,7 @@ const styles = (theme) => ({
   root: {
     display: "flex",
   },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
+
   hide: {
     display: "none",
   },
@@ -55,7 +38,7 @@ const styles = (theme) => ({
   },
   content: {
     flexGrow: 1,
-    height: "calc(100vh -64px)",
+    height: "calc(100vh - 64px)",
     padding: theme.spacing.unit * 3,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
