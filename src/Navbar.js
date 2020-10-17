4abc35a40de1d862d2306a2c526ceddaf1bf728d
Akshay Gupta <akshaygupta0795@gmail.com>
2020-10-17 15:58:30 +0530

    Add navbar and footer to singleColor palette

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react"
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom"
 import Slider from "rc-slider"
 import Select from "@material-ui/core/Select"
 import MenuItem from "@material-ui/core/MenuItem"
@@ -29,24 +29,26 @@ class Navbar extends Component {
 
   render() {
     const { format, open } = this.state
-    const { level, changeLevel } = this.props
+    const { level, changeLevel, showingAllColors } = this.props
     return (
       <header className="Navbar">
         <div className="logo">
           <Link to="/">reactcolorpicker</Link>
         </div>
-        <div className="slider-container">
-          <span>Level: {level}</span>
-          <div className="slider">
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-            />
+        {showingAllColors && (
+          <div className="slider-container">
+            <span>Level: {level}</span>
+            <div className="slider">
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+              />
+            </div>
           </div>
-        </div>
+        )}
         <div className="select-container">
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
 import React, { Component } from "react"
+import Navbar from "./Navbar"
+import PaletteFooter from "./PaletteFooter"
 import ColorBox from "./ColorBox"
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props)
+    this.state = { format: "hex" }
     this._shades = this.gatherShades(this.props.palette, this.props.colorId)
   }
 
@@ -20,19 +23,27 @@ class SingleColorPalette extends Component {
     return shades.slice(1)
   }
 
+  changeFormat = (format) => {
+    this.setState({ format })
+  }
+
   render() {
-    const colorBoxes = this._shades.map(color => (
+    const { format } = this.state
+    const { palette } = this.props
+    const { paletteName, emoji } = palette
+    const colorBoxes = this._shades.map((color) => (
       <ColorBox
         key={color.id}
         name={color.name}
-        background={color.hex}
+        background={color[format]}
         showLink={false}
       />
     ))
     return (
       <div className="Palette">
-        <h1>Single Color Palette</h1>
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <div className="Palette-colors">{colorBoxes}</div>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     )
   }
